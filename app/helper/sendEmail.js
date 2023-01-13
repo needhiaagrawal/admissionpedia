b5c7d4d43f3b8e94c9f4758f14c5a5dc64cef49b
Needhi <needhi@seosaph.com>
2023-01-13 11:45:59 +0530

    fixes for signup api

--- app/helper/sendEmail.js
+++ app/helper/sendEmail.js
@@ -2,16 +2,17 @@ const apiKey = process.env.MAIL_GUN_API_KEY;
 const domain = process.env.MAIL_GUN_DOMAIN;
 
 const mailgun = require('mailgun-js')({ domain, apiKey });
+import logger from '../../config/loggerconfig';
 
-export const sendEmail = (email, body, subject) => {
+export const sendEmail = async(email, body, subject) => {
   mailgun.messages().send({
     from: `test@${domain}`,
       to: email,
       subject,
       html: body
   }).
-  then(res => console.log('res---', res)).
-  catch(err => console.err('err-', err));
+  then(res => logger.info(`res---${res}`)).
+  catch(err => logger.error(`Error in sending email-${ err }`));
 }
 
 
@@ -22,6 +23,5 @@ export const sendEmailOtp = async (email, otp, name) => {
     <p> <b>${otp}</b> is the confirmation pin for your account, This is only valid for next 5 minutes </p>
     <p>Cheers!</p>
   `
-
   await sendEmail(email, body, subject);
 }
