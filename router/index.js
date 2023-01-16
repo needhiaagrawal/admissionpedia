77cd6f01ea986da5b1e1984b4f1f8d25293e243f
Needhi <needhi@seosaph.com>
2023-01-16 14:54:49 +0530

    forms router missed during merge code

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import locationRoute from './location';
 import schoolRoute from './school';
 import userRouter from './user';
+import formsRouter from './forms';
 import classRouter from './class';
 
 var router = express.Router();
@@ -9,6 +10,7 @@ var router = express.Router();
 router.use('/location', locationRoute);
 router.use('/school', schoolRoute);
 router.use('/user', userRouter);
+router.use('/forms', formsRouter);
 router.use('/class', classRouter);
 
 router.get('/health-check', (req, res) => {
